a8faba6f19e7225a8b2c1ad36e6154427b8c2cbc
Michael McCullough <michael.mccullough21@gmail.com>
2019-09-02 16:19:00 +0000

    Automatically backed up by Learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -2,8 +2,12 @@ class Pokemon
   attr_accessor :id, :name, :type, :db
 
 def initialize(keywords)
-
+  
 end
 
-
+def self.save(name, type, db)
+  sql = <<-SQL
+  INSERT INTO pokemon (name, type)
+  VALUES (name, type)
+  SQL 
 end
